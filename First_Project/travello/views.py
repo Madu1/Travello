6d0ba47ef88682d9380a0b2b6bd5744c2f0aca7a
Maduhansi Lakshini <maduhansilakshini@gmail.com>
2022-06-21 19:49:45 +0530

    Specify the access to destination pages

--- First_Project/travello/views.py
+++ First_Project/travello/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.contrib import auth, messages
+from django.shortcuts import render, redirect
 from .models import Destination
+from django.contrib import messages
+
 
 # Create your views here.
 def index(request):
 
     dests = Destination.objects.all()
     return render(request, 'index.html', {'dests': dests})
+
+
+def destinations(request):
+    if request.user.is_authenticated:
+        print('User is logged in..')
+        # messages.info(request, 'User is logged in..')
+        return render(request, 'destinations.html')
+    else:
+        # print('User is not logged in..')
+        messages.info(request,'User is not logged in..')
+        return render(request,'destinations.html')
+    # return render(request, 'destinations.html')
